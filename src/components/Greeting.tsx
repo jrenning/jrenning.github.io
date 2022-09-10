25f307a52901e8a843962478ef92349ae05e420c
jrenning <jackalexrenning@gmail.com>
2022-09-10 13:17:26 -0500

    Updated background to cover full screen on dark mode.

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -4,7 +4,7 @@ function Greeting() {
   return (
     <div className="font-bold text-secondary_color   flex flex-col justify-center items-center  mt-[150px]">
       <span className="text-5xl md:text-6xl">Jack Renning</span>
-      <div className=" h-[2px] bg-black md:w-[400px] w-[300px] mt-2"></div>
+      <div className=" h-[2px] bg-black md:w-[400px] w-[300px] mt-2 dark:bg-white"></div>
       <div className='text-lg'>Engineer & Programmer</div>
     </div>
   );
