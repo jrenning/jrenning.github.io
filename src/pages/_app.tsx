0a270158d8bcd6aada4336eba6c2d25794224358
jrenning <jackalexrenning@gmail.com>
2022-09-12 11:42:50 -0500

    Fixed import error.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Header from '../components/Header';
-import {useTheme} from "../hooks/dark.js"
+import {useTheme} from "../hooks/dark"
 
 function MyApp({ Component, pageProps }: AppProps) {
   useTheme()
