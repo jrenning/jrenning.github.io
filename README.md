6984423610fb70e498ddf67d31bb89247adbc313
jrenning <jackalexrenning@gmail.com>
2022-09-18 17:05:05 -0500

    Updated contact page.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-This message means that the site is currently down right now, sorry!
+This is the repository for my portfolio website. The code is split up into three
+branches.
+
+main - the current code that is built and deployed on production
+dev - code that is not done and will be merged in later 
+prod - the built code from main, this code is the code deployed on github pages
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -5,34 +5,45 @@ import PageHeader from "../components/shared/PageHeader";
 function ContactPage() {
   const { register, handleSubmit } = useForm();
 
-  const fields = ["Name", "Company", "Email"];
 
-  const contactSubmit = () => {};
+
+  const contactSubmit = () => {
+
+  };
   return (
     <div>
       <PageHeader name="Contact" />
       <form
         className="grid items-center p-12 mx-12 mt-8 shadow-md bg-tertiary_color justify-evenly dark:text-white"
         onSubmit={handleSubmit(contactSubmit)}
+        method="post"
+        action="https://getform.io/f/57c98ca4-7169-452f-a212-ea13605437b6"
       >
         <div className="grid grid-cols-1 gap-8 font-semibold text-black md:grid-cols-2">
           <div className="flex flex-col">
             <div className="flex flex-col">
-              {fields.map((field) => (
-                <React.Fragment key={field}>
-                  <label className="standard-label">{field}</label>
-                  <input
-                    {...register(field)}
-                    className="h-8 p-2 rounded-md lg:h-12 dark:text-white"
-                  ></input>
-                </React.Fragment>
-              ))}
+              <label className="standard-label">Name</label>
+              <input
+                {...register("Name")}
+                className="h-8 p-2 rounded-md lg:h-12 dark:bg-white"
+              ></input>
+              <label className="standard-label">Company</label>
+              <input
+                {...register("Company")}
+                className="h-8 p-2 rounded-md lg:h-12 dark:bg-white"
+              ></input>
+              <label className="standard-label">Email</label>
+              <input
+                {...register("Email")}
+                type="email"
+                className="h-8 p-2 rounded-md lg:h-12 dark:bg-white"
+              ></input>
             </div>
           </div>
           <div className="flex flex-col">
             <label className="standard-label">Message</label>
             <textarea
-              className={`lg:flex-1 2xl:w-[40rem] xl:w-[30rem] lg:w-[20rem] rounded-md p-2 dark:text-white`}
+              className={`lg:flex-1 2xl:w-[40rem] xl:w-[30rem] lg:w-[20rem] rounded-md p-2 dark:bg-white`}
             ></textarea>
             <div className="flex justify-center">
               <button type="submit" className="mt-4 button-standard-primary">
